d78180c9259f1f9ad695087a7192bf327f981345
qipaworld <shengming.qi.cn@gmail.com>
2017-12-17 23:00:43 +0800

    添加简单的数据挂载组件

---
+++ Scripts/Datas/SimpleData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleData : MonoBehaviour {
+
+    public string name = "";
+    public int num = 0;
+    public bool ready = false;
+}
