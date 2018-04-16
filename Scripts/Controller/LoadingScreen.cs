15d220a4150c0557937e5ac1fb9145e0572033e4
齐胜明 <shengming.qi.cn@gmail.com>
2018-04-16 20:12:22 +0800

    过度场景调整

--- Scripts/Controller/LoadingScreen.cs
+++ Scripts/Controller/LoadingScreen.cs
@@ -17,7 +17,7 @@ public class LoadingScreen : MonoBehaviour {
     private IEnumerator LoadScene()
     {
         AsyncOperation asyncOperation = null;
-        //float progress = 0;
+        float progress = 0;
 
         if (DataManager.instance == null)
         {
@@ -28,47 +28,47 @@ public class LoadingScreen : MonoBehaviour {
         }
         // u3d 5.3之后使用using UnityEngine.SceneManagement;加载场景
         // 不允许加载完毕自动切换场景，因为有时候加载太快了就看不到加载进度条UI效果了
-        //asyncOperation.allowSceneActivation = false;
+        asyncOperation.allowSceneActivation = false;
         // mAsyncOperation.progress测试只有0和0.9(其实只有固定的0.89...)
         // 所以大概大于0.8就当是加载完成了
         //while (!asyncOperation.isDone && asyncOperation.progress < 0.8f)
         //{
         //yield return asyncOperation;
         //}
+        // while (!asyncOperation.isDone)
+        // {
+        //     //SetLoadingPercentage(op.progress * 100);
+        //     //if (asyncOperation.progress > 0.95 && progress <= asyncOperation.progress)
+        //     //{
+        //     //    progress += 0.01f;
+        //     //}
+        //     //else if (progress <= asyncOperation.progress)
+        //     //{
+        //     //    progress += 0.001f;
+        //     //}
+        //     loading.fillAmount = asyncOperation.progress;
+        //     yield return new WaitForEndOfFrame();
+        // }
+
+
+        while (asyncOperation.progress < 0.9f)
+        {
+
+           while (progress < asyncOperation.progress)
+           {
+               progress += 0.05f;
+               loading.fillAmount = progress;
+               yield return new WaitForEndOfFrame();
+           }
+        }
+        asyncOperation.allowSceneActivation = true;
+
         while (!asyncOperation.isDone)
         {
-            //SetLoadingPercentage(op.progress * 100);
-            //if (asyncOperation.progress > 0.95 && progress <= asyncOperation.progress)
-            //{
-            //    progress += 0.01f;
-            //}
-            //else if (progress <= asyncOperation.progress)
-            //{
-            //    progress += 0.001f;
-            //}
-            loading.fillAmount = asyncOperation.progress;
-            yield return new WaitForEndOfFrame();
+           // progress += 0.01f;
+           loading.fillAmount = asyncOperation.progress;
+           yield return new WaitForEndOfFrame();
         }
-
-
-        //while (asyncOperation.progress < 0.9f)
-        //{
-
-        //    while (progress < asyncOperation.progress)
-        //    {
-        //        progress += 0.01f;
-        //        loading.fillAmount = progress;
-        //        yield return new WaitForEndOfFrame();
-        //    }
-        //}
-        //asyncOperation.allowSceneActivation = true;
-
-        //while (progress < 1)
-        //{
-        //    progress += 0.01f;
-        //    loading.fillAmount = progress;
-        //    yield return new WaitForEndOfFrame();
-        //}
     }
 
     void Update()
