b9b085b9e9e6528bde9775daa8160e2fc5d63a98
qipaworld <shengming.qi.cn@gmail.com>
2018-07-08 17:57:01 +0800

    引导

--- Scripts/Controller/SwitchButtonEx.cs
+++ Scripts/Controller/SwitchButtonEx.cs
@@ -9,5 +9,13 @@ public class SwitchButtonEx : MonoBehaviour {
     public void switchObj(){
         obj1.SetActive(true);
         obj2.SetActive(false);
+        if (obj2.name == "Scroll View" && DataManager.Instance.getData("GameStatus").GetNumberValue("GameHint") == 4)
+        {
+            DataManager.Instance.getData("GameStatus").SetNumberValue("GameHint", 5);
+        }
+        if (obj2.name == "Scroll View2" && DataManager.Instance.getData("GameStatus").GetNumberValue("GameHint") == 6)
+        {
+            DataManager.Instance.getData("GameStatus").SetNumberValue("GameHint", 7);
+        }
     }
 }
