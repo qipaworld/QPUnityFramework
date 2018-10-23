b77e8394460d8fabb552ce910e18ee3e3f3909dc
齐胜明 <shengming.qi.cn@gmail.com>
2018-10-23 14:32:43 +0800

    删除无用代码

--- Scripts/FireBase/FireBaseManager.cs
+++ Scripts/FireBase/FireBaseManager.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 //using Firebase.Invites;
-using System.Threading.Tasks;
 using System;
 using Firebase.Database;
 using YamlDotNet.Serialization;
