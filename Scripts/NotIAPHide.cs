45ae048c094ace679c80e8537a909ffeab83b70a
qipaworld <shengming.qi.cn@gmail.com>
2019-01-24 12:33:53 +0800

    添加屏蔽ad 和内购功能

---
+++ Scripts/NotIAPHide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotIAPHide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+#if QIPAWORLD_NOTIAP
+        gameObject.SetActive(false);
+#endif
+    }
+}
