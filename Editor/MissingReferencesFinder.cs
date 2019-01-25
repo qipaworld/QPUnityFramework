b1130a2e120f273a6d7ee9b4fc571ecb6d23ec16
qipaworld <shengming.qi.cn@gmail.com>
2019-01-25 17:39:17 +0800

    添加将大图打碎，并且重新分配游戏对象引用插件

---
+++ Editor/MissingReferencesFinder.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+/// <summary>
+/// A helper editor script for finding missing references to objects.
+/// </summary>
+public class MissingReferencesFinder : MonoBehaviour
+{
+    private const string MENU_ROOT = "QP/Missing References/";
+
+    /// <summary>
+    /// Finds all missing references to objects in the currently loaded scene.
+    /// </summary>
+    [MenuItem(MENU_ROOT + "Search in scene", false, 50)]
+    public static void FindMissingReferencesInCurrentScene()
+    {
+        var sceneObjects = GetSceneObjects();
+        FindMissingReferences(EditorSceneManager.GetActiveScene().path, sceneObjects);
+    }
+
+    /// <summary>
+    /// Finds all missing references to objects in all enabled scenes in the project.
+    /// This works by loading the scenes one by one and checking for missing object references.
+    /// </summary>
+    [MenuItem(MENU_ROOT + "Search in all scenes", false, 51)]
+    public static void MissingSpritesInAllScenes()
+    {
+        foreach (var scene in EditorBuildSettings.scenes.Where(s => s.enabled))
+        {
+            EditorSceneManager.OpenScene(scene.path);
+            FindMissingReferencesInCurrentScene();
+        }
+    }
+
+    /// <summary>
+    /// Finds all missing references to objects in assets (objects from the project window).
+    /// </summary>
+    [MenuItem(MENU_ROOT + "Search in assets", false, 52)]
+    public static void MissingSpritesInAssets()
+    {
+        var allAssets = AssetDatabase.GetAllAssetPaths().Where(path => path.StartsWith("Assets/")).ToArray();
+        var objs = allAssets.Select(a => AssetDatabase.LoadAssetAtPath(a, typeof(GameObject)) as GameObject).Where(a => a != null).ToArray();
+
+        FindMissingReferences("Project", objs);
+    }
+
+    private static void FindMissingReferences(string context, GameObject[] objects)
+    {
+        foreach (var go in objects)
+        {
+            var components = go.GetComponents<Component>();
+
+            foreach (var c in components)
+            {
+                // Missing components will be null, we can't find their type, etc.
+                if (!c)
+                {
+                    Debug.LogError("Missing Component in GO: " + GetFullPath(go), go);
+                    continue;
+                }
+
+                SerializedObject so = new SerializedObject(c);
+                var sp = so.GetIterator();
+
+                // Iterate over the components' properties.
+                while (sp.NextVisible(true))
+                {
+                    if (sp.propertyType == SerializedPropertyType.ObjectReference)
+                    {
+                        if (sp.objectReferenceValue == null
+                            && sp.objectReferenceInstanceIDValue != 0)
+                        {
+                            ShowError(context, go, c.GetType().Name, ObjectNames.NicifyVariableName(sp.name));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static GameObject[] GetSceneObjects()
+    {
+        // Use this method since GameObject.FindObjectsOfType will not return disabled objects.
+        return Resources.FindObjectsOfTypeAll<GameObject>()
+            .Where(go => string.IsNullOrEmpty(AssetDatabase.GetAssetPath(go))
+                   && go.hideFlags == HideFlags.None).ToArray();
+    }
+
+    private static void ShowError(string context, GameObject go, string componentName, string propertyName)
+    {
+        var ERROR_TEMPLATE = "Missing Ref in: [{3}]{0}. Component: {1}, Property: {2}";
+
+        Debug.LogError(string.Format(ERROR_TEMPLATE, GetFullPath(go), componentName, propertyName, context), go);
+    }
+
+    private static string GetFullPath(GameObject go)
+    {
+        return go.transform.parent == null
+            ? go.name
+                : GetFullPath(go.transform.parent.gameObject) + "/" + go.name;
+    }
+}
---
+++ Editor/SplitAtlasReLocate.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// 检查GameObject上的Image组件是否引用了丢失的精灵资源。
+/// </summary>
+public class SplitAtlasReLocate 
+{
+
+    [MenuItem("QP/Split Atlas re-locate")]
+    static void CheckMissing()
+    {
+        var selectedGameObjects = Selection.GetFiltered(typeof(GameObject), SelectionMode.TopLevel);
+        
+        if (selectedGameObjects.Length <= 0)
+        {
+            Debug.LogError("请先选择GameObject，再执行此导出指令。");
+            return;
+        }
+            
+        foreach (GameObject rootGameObject in selectedGameObjects)
+        {
+            bool includeInactive = true;
+            SpriteRenderer[] allImages = rootGameObject.GetComponentsInChildren<SpriteRenderer>(includeInactive);
+            //int missingCount = 0;
+            for (int i = 0; i < allImages.Length; i++)
+            {
+                var image = allImages[i];
+                Sprite sprite = image.sprite;
+                if (ReferenceEquals(sprite, null))
+                {
+                    // 编辑器下永远不会返回C#引用null。这条log永远不会出现。
+                    string path = GetPath(rootGameObject.transform, image.transform);
+                    Debug.Log("找到一个sprite属性为null的Image组件。点击这条消息以定位。" + path, image);
+                }
+                else
+                {
+                    // UnityEngine.Object 自定义的相等性检查。
+                    // 在编辑器环境下，Missing状态的C#对象引用非null，但是UnityEngine.Object 自定义的相等性检查与null比较返回true
+                    if (sprite == null)
+                    {
+                        // 尝试访问这个sprite的name属性时，将会返回两种不同的异常。
+                        try
+                        {
+                            string name = sprite.name;
+                        }
+                        catch (MissingReferenceException)
+                        {
+                            //引用的资源丢失，已经不在工程中。Inspector面板的Missing状态。
+                            string path = GetPath(rootGameObject.transform, image.transform);
+                            Debug.Log("找到一个sprite属性为Missing的Image组件。点击这条消息以定位。" + path, image);
+                        }
+                        catch (UnassignedReferenceException)
+                        {
+                            //完全没有赋值。Inspector面板展示为None状态。
+                        }
+
+                    }
+                    else
+                    {
+                        string spName = sprite.name;
+                        string spPath = AssetDatabase.GetAssetPath(sprite);
+                        string rootPath = Path.GetDirectoryName(spPath) + "/QPSplitAtlasOut" + Path.GetFileNameWithoutExtension(spPath);
+                        if (Directory.Exists(rootPath))
+                        {
+                            Undo.RecordObject(image, "re-locate sprte");
+                            image.sprite = AssetDatabase.LoadAssetAtPath<Sprite>(rootPath + "/" + sprite.name + ".png");
+                            if (rootGameObject.scene.rootCount == 0)
+                            {
+                                AssetDatabase.SaveAssets();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        Debug.Log("SplitAtlasReLocate...OK");
+    }
+
+    /// <summary>
+    /// 获取<paramref name="node"/>节点相对<paramref name="root"/>的路径
+    /// </summary>
+    /// <param name="root">根节点</param>
+    /// <param name="node">目标节点</param>
+    /// <returns>返回路径</returns>
+    public static string GetPath(Transform root, Transform node)
+    {
+        List<string> names = new List<string>();
+        while (node != null && node != root)
+        {
+            names.Add(node.name);
+            node = node.parent;
+        }
+
+        if (node == null)
+        {
+            throw new Exception("目标节点不是根节点的子物体！");
+        }
+
+        names.Add(root.name);
+
+        names.Reverse();
+        return string.Join("/", names.ToArray());
+    }
+}
