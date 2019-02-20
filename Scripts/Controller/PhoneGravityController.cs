33b4be86827c13434e048302388febf185831c4c
齐胜明 <shengming.qi.cn@gmail.com>
2019-02-20 22:50:17 +0800

    重力添加2D

--- Scripts/Controller/PhoneGravityController.cs
+++ Scripts/Controller/PhoneGravityController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class PhoneGravityController : MonoBehaviour
 {
     public Text testTex  = null;
-
+    public bool is2D = true;
     // Update is called once per frame
     void Update()
     {
@@ -17,7 +17,12 @@ public class PhoneGravityController : MonoBehaviour
         if (QipaWorld.Utils.IsPhone())
         {
 #if !UNITY_EDITOR
-            Physics.gravity = Input.acceleration;
+            if(is2D){
+
+                Physics.gravity = new Vector3(Input.acceleration.x*2,Input.acceleration.y*2,0);
+            }else{
+                Physics.gravity = Input.acceleration;
+            }
 #endif
         }
     }
