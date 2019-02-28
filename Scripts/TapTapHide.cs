ca7ba09786877f3e9c26836923250301a607df00
qipaworld <shengming.qi.cn@gmail.com>
2019-02-28 14:25:07 +0800

    添加taptap

---
+++ Scripts/TapTapHide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTapHide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IAPManager.Instance.isTapTap)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
